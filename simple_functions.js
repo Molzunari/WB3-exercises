7ad5ca0038b2707269d60fe488db045724d55458
Chad Jackson <Chadster6464@gmail.com>
2022-10-03 11:55:53 -0400

    Corrections

--- simple_functions.js
+++ simple_functions.js
@@ -1,17 +1,18 @@
 function favoriteThings(){
-    console.log("Chad");
-    console.log("Ratatouille");
-    console.log("Tame Impala");
+    console.log("NAME: Chad");
+    console.log("MOVIE: Ratatouille");
+    console.log("BAND: Tame Impala");
 }
 
 function whyImHere(){
-    console.log("I wanted to try out IT for the first time and see where I can come to there since my experience was mainly CS experiece. ")
+    console.log("WHY: I wanted to try out IT for the first time and very determined to get a stable job out of it. ");
 }
 
 function favoritePlace(){
-    console.log("My favorite place to visit is probably Stone Mountain Park. ")
+    console.log("PLACE: My favorite place to visit is probably Stone Mountain Park. ");
 }
 
+//CALL ALL THREE FUNCTIONS BY NAME
 favoriteThings();
 whyImHere();
 favoritePlace();
