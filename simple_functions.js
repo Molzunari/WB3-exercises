04551772de507eb289e90dbf5629549d63eca5bd
Chad Jackson <Chadster6464@gmail.com>
2022-10-03 11:33:53 -0400

    Listed favorite things

---
+++ simple_functions.js
@@ -0,0 +1,17 @@
+function favoriteThings(){
+    console.log("Chad");
+    console.log("Ratatouille");
+    console.log("Tame Impala");
+}
+
+function whyImHere(){
+    console.log("I wanted to try out IT for the first time and see where I can come to there since my experience was mainly CS experiece. ")
+}
+
+function favoritePlace(){
+    console.log("My favorite place to visit is probably Stone Mountain Park. ")
+}
+
+favoriteThings();
+whyImHere();
+favoritePlace();
