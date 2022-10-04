8e9b51cd38c20cc7143246009e5c02abb8e87244
Chad Jackson <Chadster6464@gmail.com>
2022-10-04 13:16:47 -0400

    Added content

---
+++ Strings/advanced_parsing.js
@@ -0,0 +1,10 @@
+
+// SECTION 2-12 EXERCISE 1
+
+// parse - VERB: cut into pieces - like paring-knife, parcel, parsec
+
+function parseAndDisplayName(name) {
+    const space = " ";
+    const STARTING_INDEX = 0;
+    const NO_MATCH = -1;
+}
